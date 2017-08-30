2a9549a5850c1adf9e3635c1d072a7bde4680ccc
Sean Chen <skypemaster007@gmail.com>
2017-08-30 14:46:13 -0700

    Remove some unnecessary code

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { checkIfAuthenticated } from '../../actions';
 
 export default (ComposedComponent) => {
     class RequireAuthentication extends Component {
@@ -22,5 +21,5 @@ export default (ComposedComponent) => {
         };
     };
 
-    return connect(mapStateToProps, { checkIfAuthenticated })(RequireAuthentication);
+    return connect(mapStateToProps)(RequireAuthentication);
 };
