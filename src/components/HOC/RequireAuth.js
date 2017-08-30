39086c3c85e27601457f8ba6d54f15567637c215
Sean Chen <skypemaster007@gmail.com>
2017-08-30 09:22:48 -0700

    Add comments to HOC/RequireAuth component

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -5,11 +5,14 @@ import { checkIfAuthenticated } from '../../actions';
 export default (ComposedComponent) => {
     class RequireAuthentication extends Component {
         componentWillMount() {
-
+            // Here, we want to check to see if `this.props.authenticated` is true
+            // If it isn't, then redirect the user back to the /signin page
         }
 
         render() {
-
+            // Here, check to see if `this.props.authenticated` is true
+            // If it isn't, then we don't want this component to return anything
+            // Else, render the component that was passed to this higher-order component
         }
     }
 
