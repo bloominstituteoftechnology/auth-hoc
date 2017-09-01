4e6ffce29eb244a1a6e26bab9e1a61a592864e4d
manishavi <manishalal972@gmail.com>
2017-09-01 13:02:09 -0500

    credentials

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -3,18 +3,27 @@ import { connect } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 export default (ComposedComponent) => {
-    class RequireAuthentication extends Component {
-        componentWillMount() {
-            // Here, we want to check to see if `this.props.authenticated` is true
-            // If it isn't, then redirect the user back to the /signin page
-        }
-
-        render() {
-            // Here, check to see if `this.props.authenticated` is true
-            // If it isn't, then we don't want this component to return anything
-            // Else, render the component that was passed to this higher-order component
-        }
+  class RequireAuthentication extends Component {
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        window.location = "/signin";
+      }
+      // Here, we want to check to see if `this.props.authenticated` is true
+      // If it isn't, then redirect the user back to the /signin page
     }
+
+    render() {
+      if (this.props.authenticated) {
+        ReactDOM.render(
+          ComposedComponent,
+          document.getElementById('root')
+        );
+      }
+      // Here, check to see if `this.props.authenticated` is true
+      // If it isn't, then we don't want this component to return anything
+      // Else, render the component that was passed to this higher-order component
+    }
+  }
 
     const mapStateToProps = (state) => {
         return { 
