f8383c50612a47af3ef37b102108ce2a5483204c
Colombianito66 <colombianito66@gmail.com>
2017-09-01 14:39:04 -0400

    same

--- src/components/signup.js
+++ src/components/signup.js
@@ -55,7 +55,7 @@ const mapStateToProps = (state) => {
 };
 
 // Make sure to correctly fill in this `connect` call
-SignUp = connect(mapStateToProps, {register})(SignUp);
+SignUp = connect(mapStateToProps, { register })(SignUp);
 
 export default reduxForm({
   form: 'signup',
