f680bd699f519179e066cb622569686e8974670a
Kia Choi <sgchoi529@email.arizona.edu>
2017-12-14 14:12:29 -0700

    Kia's Commit - Not working on signup.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -49,7 +49,7 @@ export const login = (username, password, history) => {
         history.push('/users');
       })
       .catch(() => {
-        dispatch(authError('Incorrect email/password combo'));
+        dispatch(authError('Incorrect username/password combo'));
       });
   };
 };
@@ -72,7 +72,7 @@ export const logout = () => {
 export const getUsers = () => {
   return dispatch => {
     axios
-      .get(`${ROOT_URL}/restricted/users`)
+      .get(`${ROOT_URL}/users`)
       .then(response => {
         dispatch({
           type: GET_USERS,
