1fbfcf8cd6a94697edecb9a5882cae73cc4548d7
Jesse Hood <jessenoob@gmail.com>
2018-01-18 16:30:09 -0500

    solved jwt

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // Fixes an issue with axios and express-session where sessions
 // would not persist between routes
 axios.defaults.withCredentials = true;
-const ROOT_URL = 'http://localhost:5000';
+const ROOT_URL = 'http://localhost:5000/api';
 
 export const USER_REGISTERED = 'USER_REGISTERED';
 export const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
@@ -27,7 +27,6 @@ export const register = (username, password, confirmPassword, history) => {
     axios
       .post(`${ROOT_URL}/users`, { username, password })
       .then(() => {
-        console.log('registered');
         dispatch({
           type: USER_REGISTERED
         });
@@ -44,7 +43,9 @@ export const login = (username, password, history) => {
   return dispatch => {
     axios
       .post(`${ROOT_URL}/login`, { username, password })
-      .then(() => {
+      .then((response) => {
+        const token = response.data.token;
+        localStorage.setItem('token', token);
         dispatch({
           type: USER_AUTHENTICATED
         });
@@ -58,23 +59,22 @@ export const login = (username, password, history) => {
 
 export const logout = () => {
   return dispatch => {
-    axios
-      .post(`${ROOT_URL}/logout`)
-      .then(() => {
-        dispatch({
-          type: USER_UNAUTHENTICATED
-        });
-      })
-      .catch(() => {
-        dispatch(authError('Failed to log you out'));
-      });
+    localStorage.removeItem('token');
+    dispatch({
+      type: USER_UNAUTHENTICATED
+    });
   };
 };
 
 export const getUsers = () => {
+  const token = localStorage.getItem('token');
   return dispatch => {
     axios
-      .get(`${ROOT_URL}/restricted/users`)
+      .get(`${ROOT_URL}/users`, { 
+        headers: {
+          Authorization: token
+        }
+      })
       .then(response => {
         dispatch({
           type: GET_USERS,
