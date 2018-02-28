7abf9e1e25ce9ff670d2465c656b10da86518e7f
Camila Daniels <camila.daniels926@gmail.com>
2018-02-28 17:58:37 -0600

    Signup/ Signin pages are working.

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Users from '../users';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
     componentWillMount() {
       // Here, we want to check to see if `this.props.authenticated` is true
+      if (!this.props.authenticated) {
+        this.props.history.push('/signin');
+      }
       // If it isn't, then redirect the user back to the /signin page
     }
 
     render() {
-      return <div />;
+      return (
       // Here, check to see if `this.props.authenticated` is true
       // If it isn't, then we don't want this component to return anything
       // Else, render the component that was passed to this higher-order component
+        <div>
+          {this.props.authenticated && <Users />}
+        </div>
+      );
     }
+    
   }
 
   const mapStateToProps = state => {
