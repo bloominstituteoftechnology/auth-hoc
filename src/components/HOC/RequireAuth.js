0dacfcee58afba8d9def90e7ab848a213ebbf750
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-02-28 16:41:57 -0500

    Implemented sign-up, working on requireauth

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
     componentWillMount() {
       // Here, we want to check to see if `this.props.authenticated` is true
       // If it isn't, then redirect the user back to the /signin page
+      if (!this.props.authenticated) {
+        <Redirect to='/signin' />
+      }
     }
 
     render() {
-      return <div />;
+      return <div />
+      // <div>
+      //   { if(!this.props.authenticated) }
+      // </div>
       // Here, check to see if `this.props.authenticated` is true
       // If it isn't, then we don't want this component to return anything
       // Else, render the component that was passed to this higher-order component
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,13 +1,16 @@
 // Complete the component in this file.
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
-import { register } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import { register } from "../actions";
 
 class SignUp extends Component {
   // This component needs a `handleFormSubmit` function that takes in
   // username, password, comfirmPassword strings as input and
   // invokes the `register` action
+  handleFormSubmit({ username, password, confirmPassword }) {
+    this.props.register(username, password, confirmPassword, this.props.history);
+  }
 
   renderAlert = () => {
     if (!this.props.error) return null;
@@ -18,7 +21,26 @@ class SignUp extends Component {
     // Use reduxForm to build the sign up form
     // Check the other components to see how reduxForm is used
     // There needs fields for Username, Password, and Confirm Password
-    return <div>Sign Up</div>;
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset>
+          <label>Email:</label>
+          <Field name="username" component="input" type="text" />
+        </fieldset>
+        <fieldset>
+          <label>Password:</label>
+          <Field name="password" component="input" type="password" />
+        </fieldset>
+        <fieldset>
+          <label>Confirm Password:</label>
+          <Field name="confirmPassword" component="input" type="password" />
+        </fieldset>
+        <button action="submit">Sign Up</button>
+        {this.renderAlert()}
+      </form>
+    );
   }
 }
 
@@ -29,9 +51,9 @@ const mapStateToProps = state => {
 };
 
 // Make sure to correctly fill in this `connect` call
-SignUp = connect(null)(SignUp);
+SignUp = connect(mapStateToProps, { register })(SignUp);
 
 export default reduxForm({
-  form: 'signup',
-  fields: ['username', 'password', 'confirmPassword']
+  form: "signup",
+  fields: ["username", "password", "confirmPassword"]
 })(SignUp);
