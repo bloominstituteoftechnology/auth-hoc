abb797f3b6c61d45d3cc4a2607ceeccce1eaac1b
Res Yeg <shaqk702@gmail.com>
2018-03-21 15:57:24 -0700

    done with HOC

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -3,16 +3,31 @@ import { connect } from 'react-redux';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
+    state = {
+      authenticated: false,
+    }
+
     componentWillMount() {
       // Here, we want to check to see if `this.props.authenticated` is true
       // If it isn't, then redirect the user back to the /signin page
+      const { history } = this.props;
+      if (!this.props.authenticated) {
+        this.setState({ authenticated: false });
+        history.push('/signin');
+      } else {
+        this.setState({ authenticated: true });
+      }
     }
 
     render() {
-      return <div />;
+      console.log(this.state.authenticated);
+      console.log(this.props.authenticated);
+      console.log(ComposedComponent);
+      return (this.state.authenticated) ? <ComposedComponent />: null;
       // Here, check to see if `this.props.authenticated` is true
       // If it isn't, then we don't want this component to return anything
       // Else, render the component that was passed to this higher-order component
+      
     }
   }
 
