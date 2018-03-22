218811893026ec095cef8fd6e8dd3aa87d5f85d3
temp <temp@Debis-Home-Mac.local>
2018-03-22 18:07:40 -0400

    Finished Assignment.

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,8 +25,9 @@ export const register = (username, password, confirmPassword, history) => {
       return;
     }
     axios
-      .post(`${ROOT_URL}/users`, { username, password })
-      .then(() => {
+      .post(`${ROOT_URL}/api/users`, { username, password })
+      .then(res => {
+        localStorage.setItem('token', res.data.token);
         dispatch({
           type: USER_REGISTERED
         });
@@ -41,9 +42,9 @@ export const register = (username, password, confirmPassword, history) => {
 export const login = (username, password, history) => {
   return dispatch => {
     axios
-      .post(`${ROOT_URL}/login`, { username, password })
-      .then((token) => {
-        localStorage.setItem('token', token);
+      .post(`${ROOT_URL}/api/login`, { username, password })
+      .then(res => {
+        localStorage.setItem('token', res.data.token);
         dispatch({
           type: USER_AUTHENTICATED
         });
@@ -60,6 +61,7 @@ export const logout = () => {
     axios
       .post(`${ROOT_URL}/logout`)
       .then(() => {
+        localStorage.removeItem('token');
         dispatch({
           type: USER_UNAUTHENTICATED
         });
@@ -71,9 +73,10 @@ export const logout = () => {
 };
 
 export const getUsers = () => {
+  const headers = { authorization: localStorage.getItem('token') }
   return dispatch => {
     axios
-      .get(`${ROOT_URL}/restricted/users`)
+      .get(`${ROOT_URL}/api/users`, { headers })
       .then(response => {
         dispatch({
           type: GET_USERS,
--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Users from '../users';
 
 export default ComposedComponent => {
   class RequireAuthentication extends Component {
