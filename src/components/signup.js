81e8b1fa959fc2c28e97cf7fe5e9b5a0e5648f45
Igor Yermak <igor.yermak@gmail.com>
2018-03-22 18:44:37 -0400

    Igor Yermak & Nathan Daniel
    First Commit

--- src/components/signup.js
+++ src/components/signup.js
@@ -8,6 +8,9 @@ class SignUp extends Component {
   // This component needs a `handleFormSubmit` function that takes in
   // username, password, comfirmPassword strings as input and
   // invokes the `register` action
+  handleFormSubmit = ({username, password, confirmPassword}) => {
+    register(this.props.username,this.props.password,this.props.confirmPassword);
+  }
 
   renderAlert = () => {
     if (!this.props.error) return null;
@@ -15,21 +18,36 @@ class SignUp extends Component {
   };
 
   render() {
-    // Use reduxForm to build the sign up form
-    // Check the other components to see how reduxForm is used
-    // There needs fields for Username, Password, and Confirm Password
-    return <div>Sign Up</div>;
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <div> Sign Up </div>
+        <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+          <fieldset>
+            <label> Username: </label>
+            <Field name="username" component="input" type="text" />
+          </fieldset>
+          <fieldset>
+            <label> Password: </label>
+            <Field name="password" component="input" type="password" />
+          </fieldset>
+          <fieldset>
+            <label> Username: </label>
+            <Field name="confirmPassword" component="input" type="password" />
+          </fieldset>
+        </form>
+      </div>
+    )
   }
 }
-
 const mapStateToProps = state => {
   return {
-    error: state.auth.error
+    error: state.auth.error,
   };
 };
 
 // Make sure to correctly fill in this `connect` call
-SignUp = connect(null)(SignUp);
+SignUp = connect(mapStateToProps, { register })(SignUp);
 
 export default reduxForm({
   form: 'signup',
