486a051615fc66110db9bad834fae993df28a44f
Aaron Burk <aarondburk@gmail.com>
2018-03-22 17:04:59 -0500

    update actions index
    - add 'api/' to register route to allow signup
    - add localStorage to login route to store JWT
    - update logout to use JWT instead of session

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,7 +25,7 @@ export const register = (username, password, confirmPassword, history) => {
       return;
     }
     axios
-      .post(`${ROOT_URL}/users`, { username, password })
+      .post(`${ROOT_URL}/api/users`, { username, password })
       .then(() => {
         dispatch({
           type: USER_REGISTERED
@@ -41,12 +41,14 @@ export const register = (username, password, confirmPassword, history) => {
 export const login = (username, password, history) => {
   return dispatch => {
     axios
-      .post(`${ROOT_URL}/login`, { username, password })
-      .then(() => {
+      .post(`${ROOT_URL}/api/login`, { username, password })
+      .then((res) => {
+        console.log(res.data.token);
         dispatch({
           type: USER_AUTHENTICATED
         });
         history.push('/users');
+        localStorage.setItem('token', res.data.token);
       })
       .catch(() => {
         dispatch(authError('Incorrect username/password combo'));
@@ -55,24 +57,29 @@ export const login = (username, password, history) => {
 };
 
 export const logout = () => {
-  return dispatch => {
-    axios
-      .post(`${ROOT_URL}/logout`)
-      .then(() => {
-        dispatch({
-          type: USER_UNAUTHENTICATED
-        });
-      })
-      .catch(() => {
-        dispatch(authError('Failed to log you out'));
-      });
-  };
+  localStorage.removeItem('token');
+  console.log('token: ', localStorage.getItem('token'));
+  return { type: USER_UNAUTHENTICATED };
+  // return dispatch => {
+  //   axios
+  //     .post(`${ROOT_URL}/logout`)
+  //     .then(() => {
+  //       dispatch({
+  //         type: USER_UNAUTHENTICATED
+  //       });
+  //     })
+  //     .catch(() => {
+  //       dispatch(authError('Failed to log you out'));
+  //     });
+  // };
 };
 
 export const getUsers = () => {
+  const token = localStorage.getItem('token');
+  console.log('token: ', token);
   return dispatch => {
     axios
-      .get(`${ROOT_URL}/restricted/users`)
+      .get(`${ROOT_URL}/api/users`, { headers: { authorization: token } })
       .then(response => {
         dispatch({
           type: GET_USERS,
