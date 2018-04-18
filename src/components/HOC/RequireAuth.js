f4c33c270b413f11ae7c1d682c6831afd31521f1
zackhitch <zackhitch@users.noreply.github.com>
2018-04-18 14:12:07 -0700

    modified RequireAuth to use BaseComponent - ZH

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default ComposedComponent => {
+const ComposedComponent = BaseComponent => {
   class RequireAuthentication extends Component {
     componentWillMount() {
       // Here, we want to check to see if `this.props.authenticated` is true
       // If it isn't, then redirect the user back to the /signin page
       if (!this.props.authenticated) {
-        this.props.history.push('./signin');
+        this.props.history.push('/signin');
       }
     }
 
     render() {
+      return <div>{this.props.authenticated ? <BaseComponent /> : null}</div>;
       // Here, check to see if `this.props.authenticated` is true
       // If it isn't, then we don't want this component to return anything
       // Else, render the component that was passed to this higher-order component
-      return (
-        <div>{this.props.authenticated ? <ComposedComponent /> : null}</div>
-      );
     }
   }
 
@@ -29,3 +27,5 @@ export default ComposedComponent => {
 
   return connect(mapStateToProps)(RequireAuthentication);
 };
+
+export default ComposedComponent;
